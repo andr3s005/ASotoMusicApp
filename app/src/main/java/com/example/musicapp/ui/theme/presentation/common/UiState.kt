a4b1da47b7eca6558d48beb4ed65beea3b1e05a4
andr3s005 <asg78886@lasallebajio.edu.mx>
2025-10-14 19:45:26 -0600

    Configuracion del UiState

--- app/src/main/java/com/example/musicapp/ui/theme/presentation/common/UiState.kt
+++ app/src/main/java/com/example/musicapp/ui/theme/presentation/common/UiState.kt
@@ -1,2 +1,7 @@
 package com.example.musicapp.ui.theme.presentation.common
 
+sealed class UiState<out T>{
+    data object Loading : UiState<Nothing>()
+    data class Success<T>(val data : T) : UiState<T>()
+    data class Error(val message : String) : UiState<Nothing>()
+}
