d0421a8c3cd9343c64540e495dfbd0bdf75df5fd
andr3s005 <asg78886@lasallebajio.edu.mx>
2025-10-15 20:14:08 -0600

    Creacion del MiniPlayer

--- app/src/main/java/com/example/musicapp/ui/theme/presentation/common/MiniPlayer.kt
+++ app/src/main/java/com/example/musicapp/ui/theme/presentation/common/MiniPlayer.kt
@@ -1,2 +1,142 @@
 package com.example.musicapp.ui.theme.presentation.common
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.SkipNext
+import androidx.compose.material.icons.filled.SkipPrevious
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.musicapp.ui.theme.data.model.Album
+
+@Composable
+fun MiniPlayer(
+    album: Album,
+    isPlaying : Boolean,
+    onTooglePlay : () -> Unit
+){
+    val progress by rememberSaveable { mutableStateOf(0.3f) }
+    val animatedProgress by animateFloatAsState(targetValue = progress, label = "ProgressAnimation")
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp),
+        shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Black.copy(alpha = 0.95f))
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            LinearProgressIndicator(
+                progress = animatedProgress,
+                color = Color.DarkGray,
+                trackColor = Color.Gray.copy(alpha = 0.5f),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(2.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                AsyncImage(
+                    model = album.imageUrl,
+                    contentDescription = "${album.title}",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    Text(
+                        album.title,
+                        color = Color.White,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp,
+                        maxLines = 1
+                    )
+                    Text(
+                        album.artist,
+                        color = Color.White.copy(alpha = 0.6f),
+                        fontSize = 12.sp,
+                        maxLines = 1
+                    )
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    IconButton(
+                        onClick = { }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.SkipPrevious,
+                            contentDescription = "Anterior",
+                            tint = Color.White
+                        )
+                    }
+
+                    FloatingActionButton(
+                        onClick = onTooglePlay,
+                        modifier = Modifier.size(48.dp),
+                        containerColor = Color.White,
+                        contentColor = Color.Black
+                    ) {
+                        Icon(
+                            imageVector = if(isPlaying) Icons.Default.Pause else Icons.Default.PlayArrow,
+                            contentDescription = if (isPlaying) "Pausar" else "Reproducir"
+                        )
+                    }
+
+                    IconButton(
+                        onClick = { }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.SkipNext,
+                            contentDescription = "Siguiente",
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
